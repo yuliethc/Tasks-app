7f369ae4c90357fb4bfa196701437ec2ab574d55
Yulieth Lubo <yuliethdotcom@gmail.com>
2022-08-03 17:35:44 +0200

    some details

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ const About = () => {
     return (
         <div>
             <h4>Version 1.0.0</h4>
-            <p>App task tracker helps to keep record for your events or taks, make sure to add the description, the date and time</p>
+            <p>App task tracker helps to keep record for your events or tasks, make sure to add the description, the date and time.</p>
             <Link to="/">Go back</Link>
         </div>
     );
