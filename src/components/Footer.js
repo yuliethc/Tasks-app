39cb9b6797525cbdd282a886f018dbaed5a1e2f1
Yulieth Lubo <yuliethdotcom@gmail.com>
2022-08-03 16:54:23 +0200

    Adding some info

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,9 @@ import { Link } from "react-router-dom";
 const Footer = () => {
     return (
         <footer>
-            <p>Copyright &copy; 2022</p>
-            <Link to="/about">About</Link>
+            <p>Copyright &copy; <Link to="https://www.yulieth.com/">yulieth.com</Link> 2022</p>
+            <p><Link to="/about">About</Link></p>
+            
         </footer>
     );
 };
