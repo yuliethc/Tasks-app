ce9b23c0bbe894a4b0ef4513bd8c8ca836e57b01
Yulieth Lubo <yuliethdotcom@gmail.com>
2022-08-03 14:21:15 +0200

    Add task

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import { useState } from "react";
+import AddTask from "./components/AddTask";
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -24,6 +25,18 @@ function App() {
     },
   ]);
 
+  const addTask = (text, day, reminder) => {
+    setTasks([
+      ...tasks,
+      {
+        id: tasks.length + 1,
+        text,
+        day,
+        reminder,
+      },
+    ]);
+  }
+
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
   };
@@ -36,6 +49,7 @@ function App() {
   return (
     <div className="container">
       <Header />
+      <AddTask addTask={addTask}/>
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
